1625cb536fc409d52ee65e8acda026c5f2ea61eb
gtraversa <gtraversa@users.noreply.github.com>
2021-12-09 18:24:35 +0000

    first commit

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import sys, getopt
 
 def main(argv):
@@ -19,3 +20,26 @@ def main(argv):
 
 if __name__ == "__main__":
    main(sys.argv[1:])
+=======
+import numpy as np
+import open3d as o3d
+import sys
+
+def plot_pcd(path):
+    
+    bin_pcd = np.fromfile(path , np.float32)
+
+    # Reshape and drop reflection values
+    points = bin_pcd.reshape((-1, 3))[:, 0:3]
+
+    # Convert to Open3D point cloud
+    o3d_pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(points))
+
+    o3d.visualization.draw_geometries([o3d_pcd]) 
+
+if __name__ == '__main__':
+    
+    path = sys.argv[1]
+
+    plot_pcd(path)
+>>>>>>> 5ed2bbc7c29dba6adb0c595a86474f19fd0fafbd
